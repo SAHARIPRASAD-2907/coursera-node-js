d7a1bd9011ae901a5b7053ea5927da410eda5a8f
HARIPRASAD S A <myharicode@gmail.com>
2021-03-27 22:19:12 +0530

    creating server with express

--- node-http/index.js
+++ node-http/index.js
@@ -1,15 +1,28 @@
 const { log } = require('console');
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
 
 const hostname = 'localhost'
 const port = 3000;
 
 const server = http.createServer((req,res)=>{
-    console.log(req.headers);
+    console.log("Request for "+req.url+"by method"+req.method);
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/html')
-    res.end('<html><body><h1>Hello World!</h1></body></html>')
+    if(req.method == 'GET'){
+        var fileUrl;
+        if(req.url=='/') fileUrl='/index.html'
+        else fileUrl = req.url;
+
+
+        var filePath = path.resolve('./public'+filePath)
+        const fileExt = path.extname(filePath)
+        if(fileExt == '.html'){
+            fs.existsSync(filePath)
+        }
+
+    }
 })
 
 server.listen(port,hostname,()=>{
