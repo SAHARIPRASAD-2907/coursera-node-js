a7e1c3b5472967821c92b062e0fd2374ab63feb3
HARIPRASAD S A <myharicode@gmail.com>
2021-03-28 16:55:57 +0530

    finish setting up express generator for mongoDB

---
+++ WEEK-2/routes/dishRouter.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const dishRouter = express.Router();
+
+
+dishRouter.use(bodyParser.json());
+
+
+//routing for dishes
+dishRouter.route('/')
+.all((req,res,next)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/plain')
+    next();
+}).get((req,res,next)=>{
+    res.end('Will send all the dishes to you')
+}).post((req,res,next)=>{
+    res.end("Will add the dish:"+req.body.name+'with details:'+req.body.description);
+}).put((req,res,next)=>{
+    res.setTimeout=403;
+    res.end('PUT operation not suported on /dishes');
+}).delete((req,res,next)=>{
+    res.end('Deleting all the dishes!')
+});
+
+
+// ROUTING FOR DISH ID
+dishRouter.route('/:dishId')
+.get((req,res,next)=>{
+    console.log(req.params);
+    res.end('Will send details of dishes:'+req.params.dishId +'to you')
+})
+.post((req,res,next)=>{
+    res.setTimeout=403;
+    res.end('POST operation not suported on /dishes/'+req.params.dishId);
+})
+.put((req,res,next)=>{
+    
+    res.write('Updating the dish'+ req.params.dishId +'\n');
+    res.end('will update the dish:' + req.body.name + ' with details:' + req.body.description)
+})
+.delete((req,res,next)=>{
+    res.end('Deleting dish'+req.params.dishId)
+});
+
+
+module.exports = dishRouter;
---
+++ WEEK-2/routes/promoRouter.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const promoRouter = express.Router();
+promoRouter.use(bodyParser.json())
+
+//routing for promotions
+promoRouter.route('/')
+.all((req,res,next)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/plain')
+    next();
+}).get((req,res,next)=>{
+    res.end('Will send all promotions to you')
+}).put((req,res,next)=>{
+    res.setTimeout=403;
+    res.end('PUT operation not suported on /promotions');
+}).post((req,res,next)=>{
+    res.end("Will add the promotions:"+req.body.name+'with details:'+req.body.description);
+}).delete((req,res,next)=>{
+    res.end('Deleting all the promotions!')
+});
+
+//ROUTING FOR PROMOTION ID
+promoRouter.route('/:promoId')
+.get((req,res,next)=>{
+    res.end('Will send details of promotion:'+req.params.promoId +'to you')
+})
+.post((req,res,next)=>{
+    res.setTimeout=403;
+    res.end('POST operation not suported on /promotions/'+req.params.promoId);
+})
+.put((req,res,next)=>{
+    
+    res.write('Updating the promotion'+ req.params.promoId +'\n');
+    res.end('will update the promotion:' + req.body.name + ' with details:' + req.body.description)
+})
+.delete((req,res,next)=>{
+    res.end('Deleting promo'+req.params.dishId)
+});
+
+module.exports=promoRouter
